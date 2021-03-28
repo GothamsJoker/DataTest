b27513c53b94345dc2269a74e53ae3707540ef22
Kenny Goodin <kennethbgoodin@gmail.com>
2021-03-28 18:57:08 -0400

    reorganize CommentDatabase

--- src/main/java/com/dylan/service/CommentsDatabase.java
+++ src/main/java/com/dylan/service/CommentsDatabase.java
@@ -10,7 +10,19 @@ import java.sql.Statement;
 import java.util.Collection;
 import java.util.HashSet;
 
+/**
+ * Allows reading and writing comments to/from a persistent database.
+ */
 public class CommentsDatabase {
+
+    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS comment(author STRING," +
+            " content STRING, pageId INT, timestamp LONG)";
+    private static final String SELECT = "SELECT author, content, pageId, timestamp FROM " +
+            "comment";
+    private static final String SELECT_BY_PAGE = SELECT + " WHERE pageId = ?";
+    private static final String INSERT = "INSERT INTO comment (author, content, pageId, " +
+            "timestamp) VALUES (?, ?, ?, ?)";
+
 
     private final Database database;
 
@@ -19,9 +31,21 @@ public class CommentsDatabase {
         createTable();
     }
 
+    private void createTable() throws SQLException {
+        // create ya fuckin table on start up
+        Statement statement = database.getConnection().createStatement();
+        statement.executeUpdate(CREATE_TABLE);
+    }
+
+    /**
+     * Saves a {@link Comment} to the database.
+     *
+     * @param bitch The comment
+     *
+     * @throws SQLException On failing to save the comment
+     */
     public void saveComment(Comment bitch) throws SQLException {
-        PreparedStatement fuck = database.prepareStatement(" insert into comment (author, content, pageId, timestamp)"
-                + " values (?, ?, ?, ?)");
+        PreparedStatement fuck = database.prepareStatement(INSERT);
         fuck.setString(1, bitch.getAuthor());
         fuck.setString(2, bitch.getContent());
         fuck.setInt(3, bitch.getPageId());
@@ -30,41 +54,52 @@ public class CommentsDatabase {
         fuck.execute();
     }
 
+    /**
+     * @return A {@link Collection} of all comments in the database.
+     *
+     * @throws SQLException On failing to read the comments.
+     */
     public Collection<Comment> readComments() throws SQLException{
-        Statement st = database.getConnection().createStatement();
-        String query = "select author, content, pageId, timestamp from comment";
-        ResultSet rs = st.executeQuery(query);
+        PreparedStatement st = database.prepareStatement(SELECT);
+        ResultSet rs = st.executeQuery();
+
         Collection<Comment> comments = new HashSet<>();
         while (rs.next()) {
-            String auth = rs.getString("author");
-            String con = rs.getString("content");
-            int page = rs.getInt("pageId");
-            long ti = rs.getInt("timestamp");
-            Comment fuck = new Comment(auth, con, page, ti);
+            Comment fuck = parseComment(rs);
             comments.add(fuck);
         }
+
         return comments;
     }
 
+    /**
+     * Reads all of the comments for a given web page from the database.
+     *
+     * @param pageId The web page id.
+     *
+     * @return A {@link Collection} of comments for the given web page.
+     *
+     * @throws SQLException On failing to read the comments.
+     */
     public Collection<Comment> readComments(int pageId) throws SQLException{
-        PreparedStatement st = database.prepareStatement("SELECT author, content, pageId, timestamp FROM comment WHERE pageId = ?");
+        PreparedStatement st = database.prepareStatement(SELECT_BY_PAGE);
         st.setInt(1, pageId);
         ResultSet rs = st.executeQuery();
+
         Collection<Comment> comments = new HashSet<>();
         while (rs.next()) {
-            String auth = rs.getString("author");
-            String con = rs.getString("content");
-            int page = rs.getInt("pageId");
-            long ti = rs.getInt("timestamp");
-            Comment fuck = new Comment(auth, con, page, ti);
+            Comment fuck = parseComment(rs);
             comments.add(fuck);
         }
         return comments;
     }
 
-    private void createTable() throws SQLException {
-        // create ya fuckin table on start up
-        Statement statement = database.getConnection().createStatement();
-        statement.executeUpdate("CREATE TABLE IF NOT EXISTS comment(author STRING, content STRING, pageId INT, timestamp LONG)");
+    private Comment parseComment(ResultSet rs) throws SQLException {
+        String auth = rs.getString("author");
+        String con = rs.getString("content");
+        int page = rs.getInt("pageId");
+        long ti = rs.getInt("timestamp");
+
+        return new Comment(auth, con, page, ti);
     }
 }
